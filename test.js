93c772373077f103134cfce7f37abaa854126736
Pakawat Muangmit <bankzxii@gmail.com>
2019-12-11 23:19:58 +0700

    add mocha and test assert

--- test.js
+++ test.js
@@ -1,4 +1,12 @@
-const math = require('./math');
-const add = math.add1;
-console.log(math.add1(2,2));
-console.log(add(2,2));
+const assert = require("assert");
+describe("file to be tested" , ()=> {
+    context("fuction to be tested", ()=>{
+        it("should do something", ()=>{
+            assert.equal(1,1);
+        });
+        it("should do another things", ()=>{
+            assert.deepEqual([1,2,3],[1,2,3]);
+            assert.deepEqual({name: 'kob'}, {name: 'kob'});
+        });
+    });
+});
