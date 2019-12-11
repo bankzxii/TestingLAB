4c55f27ebe3f736ea8288add40ff4f8f3881981d
Pakawat Muangmit <bankzxii@gmail.com>
2019-12-11 23:02:37 +0700

    add file test.js

--- math.js
+++ math.js
@@ -1,5 +1,14 @@
-function add(a, b) {
+function add1(a, b) {
     return a+b;
 }
+const add2 = function(a, b) {
+    return a+b;
+};
+const add3 = (a, b) => {
+    return a+b;
+};
 
-console.log(add(1,1));
+module.exports = {
+    add1,
+    add2
+}
